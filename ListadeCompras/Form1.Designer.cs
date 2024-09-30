63ce0771ec45b24678458dc53148d1288f10124e
marcoshjunior <155772166+marcoshjunior@users.noreply.github.com>
2024-09-30 19:26:57 -0300

    Add project files.

---
+++ ListadeCompras/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ListadeCompras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxProdutos = new System.Windows.Forms.ListBox();
+            this.lbxCompras = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNovaCompra = new System.Windows.Forms.Button();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxProdutos
+            // 
+            this.lbxProdutos.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxProdutos.FormattingEnabled = true;
+            this.lbxProdutos.ItemHeight = 18;
+            this.lbxProdutos.Location = new System.Drawing.Point(12, 29);
+            this.lbxProdutos.Name = "lbxProdutos";
+            this.lbxProdutos.Size = new System.Drawing.Size(208, 202);
+            this.lbxProdutos.TabIndex = 0;
+            // 
+            // lbxCompras
+            // 
+            this.lbxCompras.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxCompras.FormattingEnabled = true;
+            this.lbxCompras.ItemHeight = 18;
+            this.lbxCompras.Location = new System.Drawing.Point(226, 29);
+            this.lbxCompras.Name = "lbxCompras";
+            this.lbxCompras.Size = new System.Drawing.Size(220, 166);
+            this.lbxCompras.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lista de Produtos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lista de Compras";
+            // 
+            // btnNovaCompra
+            // 
+            this.btnNovaCompra.Location = new System.Drawing.Point(12, 234);
+            this.btnNovaCompra.Name = "btnNovaCompra";
+            this.btnNovaCompra.Size = new System.Drawing.Size(89, 27);
+            this.btnNovaCompra.TabIndex = 5;
+            this.btnNovaCompra.Text = "Comprar";
+            this.btnNovaCompra.UseVisualStyleBackColor = true;
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Location = new System.Drawing.Point(360, 234);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(85, 27);
+            this.btnFinalizar.TabIndex = 6;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BackColor = System.Drawing.Color.White;
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTotal.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(226, 198);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(219, 33);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "Lista de Compras";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 273);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.btnNovaCompra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxCompras);
+            this.Controls.Add(this.lbxProdutos);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxProdutos;
+        private System.Windows.Forms.ListBox lbxCompras;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNovaCompra;
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ ListadeCompras/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListadeCompras
+{
+    public partial class Form1 : Form
+    {
+        class produto
+        {
+            public string nome;
+            public double preco;
+        }
+
+        //contem os produtos da lista produto
+        List<produto> listaProdutos;
+        //contem os produtos da lista compra
+        List<produto> listaCompras;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
